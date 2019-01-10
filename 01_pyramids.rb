bd4f561a2551c552200a37ee0ba3995833712883
theofr0198 <46031428+theofr0198@users.noreply.github.com>
2019-01-10 15:45:10 +0100

    Add files via upload
    pyramid fini

--- 01_pyramids.rb
+++ 01_pyramids.rb
@@ -67,6 +67,56 @@ def full_pyramid
 end
 end
 
-half_pyramid
 
+def wtf_pyramid
+puts "rentrez un nombre compris entre 1 et 25"
+	nbr = gets.chomp.to_i
+
+	#Calcul du nombre d'espace
+
+
+	i = 1
+	while (i <= nbr)
+	temp = nbr - (i/2)
+	temp.times do 
+	print " "
+	end
+
+
+
+
+#Calcul du nombre de diez
+    i.times do
+		print "#"
+	end
+	
+	print "\n"
+	i += 2
+
+end
+
+i = nbr -2
+	while (i > 0 )
+	temp = nbr - (i/2)
+	temp.times do 
+	print " "
+	end
+
+
+
+
+#Calcul du nombre de diez
+    i.times do
+		print "#"
+	end
+	
+	print "\n"
+	i += -2
+
+
+end
+end
+
+
+wtf_pyramid
 
